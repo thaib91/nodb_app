43d7d5f6f7fd8cd7662c6e83bc94240fe6af95b0
Thai Bui <ThaiBui@Thais-MacBook-Pro.local>
2018-12-07 17:22:57 -0700

    first commit

---
+++ src/components/UpdatePost.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class  extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            text: props.text
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
