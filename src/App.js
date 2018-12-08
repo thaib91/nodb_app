8d9ed7bd44a80595434c40519b99ee21427bbf10
Thai Bui <ThaiBui@Thais-MacBook-Pro.local>
2018-12-07 17:22:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './App.css'
+import Comments from './components/comments/Comments'
+import UpdatePost from './components/UpdatePost'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      memes: [],
+      comments: [],
+      userInput: " ",
+      randomMeme: [],
+      editedInput:""
+
+    }
+    this.createPost=this.createPost.bind(this)
+  }
+
+  componentDidMount() {
+    axios.get(`/api/memes`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          randomMeme: [res.data]
+        })
+      })
+
+      this.getPost()
+  }
+
+  randomClick() {
+    axios.get(`/api/memes`)
+      .then(res => {
+        console.log(res)
+        this.setState({
+          randomMeme: [res.data],
+        })
+
+      })
+  }
+
+  // editPost(updateID){
+  //   axios.put(`/api/memes/:id/${updateID}`, {text})
+  //     .then(res => {
+  //       console.log(res)
+  //       this.setState({
+  //         comments: res.data
+  //       })
+  //     })
+  // }
+
+
+
+  createPost(text){
+    axios.post(`/api/meme`, {text})
+      .then(res =>{
+        console.log(res)
+        this.setState({
+          comments: res.data
+        })
+      })
+  }
+
+  getPost(){
+    axios.get(`/api/meme`)
+      .then(res=>{
+        this.setState({
+          comments: res.data
+        })
+      })
+  }
+
+  deletePost(deleteID){
+    axios.delete(`/api/meme/${deleteID}`)
+      .then(res=>{
+        this.setState({
+          comments: res.data
+        })
+      })
+  }
+
+
+
+  handleChange(prop, val) {
+    this.setState({
+      [prop]: val
+    })
+  }
+
+
   render() {
+
+
+    const displayMeme = this.state.randomMeme.map((memeOne) => {
+      return (
+          <div key={memeOne}>
+            <img onClick={() => this.randomClick()}  className="images" src={memeOne.meme} alt="" />
+          </div>
+      )
+
+
+    })
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {displayMeme}
+
+      <Comments createPostFn={this.createPost}/>
+    
+        {this.state.comments.map((comment)=>{
+          console.log(comment)
+          return(
+            <div key={comment.id}>
+              {comment.text}
+              <div>
+              <button onClick={()=>this.deletePost(comment.id)}>Delete</button>
+              <button onClick={()=>this.editPost(comment.id)}>Edit</button>
+              </div>
+            </div>
+          )
+        })}
+
+
+     
+        {/* <div>
+            <button className="randomButton" onClick={() => this.randomClick()}></button>
+          </div> */}
+
       </div>
     );
   }
